1552fb97be731e390ec8e7d60e5631dbd9a6ef6b
Magnus <68356591+manezz@users.noreply.github.com>
2020-08-13 13:15:08 +0200

    13/8

--- Order_Program/Program.cs
+++ Order_Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Security.Cryptography;
@@ -37,6 +38,9 @@ namespace Order_Program
                 }
 
                 string svar = Console.ReadLine();
+                string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(svar.ToLower());
+
+                List<Pizza> yourOrders = new List<Pizza> { };
 
                 try
                 {
@@ -45,23 +49,22 @@ namespace Order_Program
                     {
                         if (p.MenuNumber == order)
                         {
-                            Console.WriteLine($"Ok har valgt nummer {order}");
-                        }
-                    }
+                            yourOrders.Add(pizzas[order -1]);
+                            Console.WriteLine(pizzas[order -1].Name);
+                        } 
+                    } 
                 }
                 catch (Exception) { }
                 finally
                 {
                     foreach (Pizza p in pizzas)
                     {
-                        if (p.Name == svar)
+                        if (p.Name.ToLower() == svar.ToLower())
                         {
-                            Console.WriteLine($"Ok har valgt {svar}");
+                            Console.WriteLine($"Ok har valgt {titleCase}");
                         }
                     }
                 }
-
-
             }
             while (i == 0);
 
